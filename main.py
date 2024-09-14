9e535e806f746bde29d04d5007e3ba5bbc4e4509
sasha6839 <saskohrapan@gmail.com>
2024-09-14 12:04:09 +0300

    python-flask

--- main.py
+++ main.py
@@ -1,7 +1,32 @@
 from flask import Flask, render_template
+import os
+from flask_sqlalchemy import SQLAlchemy
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    __tablename_ = 'User'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(150), nullable=False, unique=True)
+    password = db.Column(db.String(25), nullable=False)
+
+    def __repr__(self):
+        return f"<user {self.username}"
+
+
+
+
+with app.app_context():
+    db.create_all()
 
 @app.route('/')
 def index():
@@ -31,6 +56,6 @@ def details():
     return render_template('details.html')
 
 
-# Лише для локаотного сервера (закоментувати)
+# Лише для локального сервера (закоментувати)
 if __name__ == '__main__':
     app.run(debug=True)
